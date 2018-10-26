fc19bf4af7ae64a0221ee7867fefac3edac4e890
Lorenzo Fresco <loryfresco@gmail.com>
2018-10-26 16:42:05 +0200

    Added comments

--- date.cc
+++ date.cc
@@ -1,25 +1,36 @@
 #include <iostream>
 
 
-
+// Creates a scoper enum to represent the months
 enum class Month{
  	jan = 1, feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec
- };
-
+ }; 
+ 
+ 
+// Functions that allows us to convert an integer into a month
 Month itom(int i);
 
+//Implementations of the concept of a date
 class Date{
-	int day, year;
+	int day, year; 
 	Month month;
 	
 	public: 
-	Date(int, Month, int);
-	int get_day(){return day;}
+	Date(int, Month, int);  //constructor
+	
+	// the following functions allow us to get the day, the month and the year
+	
+	int get_day(){return day;} 
 	Month get_month(){return month;}
 	int get_year(){return year;}
+	
+	// Functions that allow us to add days to our date. 
+	
 	void add_days(const unsigned int n);
 	void add_day(){Date::day += 1;}
 };
+
+//Implementations of the contructor
 
 Date::Date(int d, Month m, int y){
 	
@@ -59,19 +70,20 @@ int main(){
  
  
  std::cout << "The day is " << data.get_day() << std::endl;
- std::cout << "The month is " << static_cast<int>(data.get_month()) << std::endl;
+ std::cout << "The month is " << data.get_month() << std::endl;
  std::cout << "The year is " << data.get_year() << std::endl;
  return 0;
  
 }
 
 
-
+//Overload of the << operator in order to print out the number of the month
 std::ostream& operator<<(std::ostream& os, Month m) {
 	
 	os << static_cast<int>(m) <<std::endl;
 	return os;
 }
+
 
 
 Month itom(int i){
@@ -88,15 +100,15 @@ Month itom(int i){
 void Date::add_days(const unsigned int n){
 
   
-  int counter = 0;
+  int counter = 0; //keeps track of the number of the days added
   
   while(counter < n){
   
-  	add_day();
+  	add_day(); //adds a day
   	
   	
   	
-  	int mese = static_cast<int>(Date::month);
+  	int mese = static_cast<int>(Date::month); // this int will help us modify the month
   	
   	switch(month){
   	
@@ -104,7 +116,7 @@ void Date::add_days(const unsigned int n){
   	case Month::jul: case Month::aug: case Month::oct:
   	if(Date::day >31){
   	
-  		Date::day = Date::day - 31;
+  		Date::day = Date::day - 31; 
   		Date::month = itom(mese + 1);
   	
   	}
